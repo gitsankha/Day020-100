cff4ab73bda6262fc2f5f1f945b7d36d26d7dd1c
gitsankha <sahasankhasubhra4@gmail.com>
2024-11-09 07:58:02 +0530

    final code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+from snake import Snake
 import time
 
 
@@ -7,35 +8,23 @@ screen = turtle.Screen()
 screen.setup(height=600,width=600)
 screen.title("My Snake Xenzia")
 screen.bgcolor("black")
-# screen.delay(100)
 screen.tracer(0,0)
-# color = ['white', 'red', 'green']
-all_turtles = []
-turtle_pos = [(0,0),(-20,0),(-40,0)]
-segments = []
-
-for i in range(0,3):
-    new_turtle = turtle.Turtle("square")
-    new_turtle.color("white")
-    new_turtle.pu()
-    new_turtle.goto(turtle_pos[i])
-    segments.append(new_turtle)
-
-
 
 is_game_on = True
+
+snake = Snake()
+screen.listen()
+
+"""START HERE """
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
 while is_game_on:
     screen.update()
-    time.sleep(1)
-    for i in range(len(segments)):
-        if i == 0:
-            segment_position = tuple(segments[i].position())
-            segments[i].left(90)
-            segments[i].fd(20)
-        else:
-            segments[i].goto(segment_position)
-            segment_position = tuple(segments[i].position())
-
+    time.sleep(0.1)
+    snake.move()
 
 
 
